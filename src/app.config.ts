4680ad24e8bb7595bd76b88ce290249bec636675
Tobi Balogun <98078707+tobisupreme@users.noreply.github.com>
2023-07-25 18:13:49 +0100

    new: set up config service

---
+++ src/app.config.ts
@@ -0,0 +1,25 @@
+import { config } from 'dotenv';
+
+config();
+
+const env = (key: string, defaultVal: any = undefined) => {
+  return process.env[key] || defaultVal;
+};
+
+env.require = (key: string, defaultVal: any = undefined) => {
+  const value = process.env[key] || defaultVal;
+  if (!value) {
+    throw new Error(`Environment variable '${key}' is missing!`);
+  }
+
+  return value;
+};
+
+const configuration = {
+  environment: env.require('NODE_ENV', 'development'),
+  app: {
+    port: parseInt(env('PORT', 8000)),
+  },
+};
+
+export default () => configuration;
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppGateway } from './app.gateway';
+import { ConfigModule } from '@nestjs/config';
+import appConfig from './app.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [appConfig],
+    }),
+  ],
   providers: [AppGateway],
 })
 export class AppModule {}
